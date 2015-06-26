c66a4ff63c5a1845c25dbfb1d2b8ed27f08f88e4
Phil Tanner <phil.tanner@sjs.co.nz>
2015-06-26 12:28:44 +1200

    Pull ads from the database instead of the ads.dat file

--- ads.inc.php
+++ ads.inc.php
@@ -23,17 +23,50 @@ define( 'PHPADS_ADELEMENT_OTHERCONTENT',14);
 define( 'PHPADS_ADTYPE_IMAGE',           0);
 define( 'PHPADS_ADTYPE_OTHER',           1);
 
+
 foreach ($lines as $line) {
     $line = chop($line);
     if (($line != '') && (!preg_match('/^#/', $line))) {
         if (preg_match('/^[A-Za-z0-9 ]+\|\|/', $line)) {
-            $ads[] = $line;
+//            $ads[] = $line;
         } else {
             list ($key, $val) = explode('=', $line);
             $bannerAds[$key] = $val;
         }
     }
 }
+if (trim(strlen($bannerAds['mysql_host'] . $bannerAds['mysql_username'] . $bannerAds['mysql_password'] . $bannerAds['mysql_database'] . (int)$bannerAds['mysql_port'])) > 0) {
+
+	$mysqli = new mysqli($bannerAds['mysql_host'], $bannerAds['mysql_username'], $bannerAds['mysql_password'], $bannerAds['mysql_database'], (int)$bannerAds['mysql_port']);
+	if ($mysqli->connect_error) {
+	    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+	}
+
+	if (mysqli_connect_error()) {
+	    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+	}
+
+	if ($result = $mysqli->query("SELECT * FROM `ads`;") ) {
+		$ads = array();
+		while ($row = $result->fetch_row()) {
+			if (is_null($row[PHPADS_ADELEMENT_ENDDATE])) {
+				$row[PHPADS_ADELEMENT_ENDDATE] = 99999999;
+			} else {
+				$row[PHPADS_ADELEMENT_ENDDATE] = strtotime($row[PHPADS_ADELEMENT_ENDDATE]);
+                        }
+			if (is_null($row[PHPADS_ADELEMENT_STARTDATE])) {
+				$row[PHPADS_ADELEMENT_STARTDATE] = 99999999;
+                        } else {
+				$row[PHPADS_ADELEMENT_STARTDATE] = strtotime($row[PHPADS_ADELEMENT_STARTDATE]);
+			}
+			$ads[] = implode("||",$row);
+		}
+		$result->close();
+	}
+
+	$mysqli->close();
+}
+
 date_default_timezone_set($bannerAds['timezone']);
 function writeads()
 {
